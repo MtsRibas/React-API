687debe5ee2888024940170f726219dae9f187e3
Matheus Ribas <matheusribas2013@gmail.com>
2024-10-03 23:08:18 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,42 @@
+class App extends Component {
+  state = {
+    postagens: [],
+  };
+
+  referencia = createRef();
+  //utilizando uma referencia para poder usar o botao do header para rolar a pagina ate essa area de referencia
+
+  adicionarPostagem = (novaPostagem) => {
+    const novaPostagemComId = {
+      ...novaPostagem,
+      id: Math.random(),
+      //aqui estamos gerando novas postagens com id random garantindo que nao tenha ids iguais
+    };
+    this.setState((post) => ({
+      postagens: [...post.postagens, novaPostagemComId],
+    }));
+  };
+
+  
+  deletarPostagem = (id) => {
+    this.setState((postagem) => ({
+      postagens: postagem.postagens.filter((postagem) => postagem.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <Main>
+        <Header Referencia={this.referencia} />
+        <Banner adicionarPostagem={this.adicionarPostagem} />
+        <ListaPostagens
+          postagens={this.state.postagens}
+          referencia={this.referencia}
+          Deletar={this.deletarPostagem}
+        />
+      </Main>
+    );
+  }
+}
+
+export default App;
